8c6b73a4b549a6a1e973a6d962f627f17770fe12
harsh0506 <joshiharsh0506@gmail.com>
2023-04-02 17:50:11 +0530

    removed credentials

--- server.js
+++ server.js
@@ -18,38 +18,6 @@ app.use("/user", routes)
 app.use("/teams", teams)
 app.use("/projects", projects)
 
-app.get("/sendEmail", async (req, res) => {
-	try {
-		sendEmail()
-		res.send("done")
-	} catch (error) {
-		res.json(error)
-	}
-
-})
-
-function sendEmail() {
-	sgMail.setApiKey("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")
-
-	const msg = {
-		to: 'snehalj16680@gmail.com', // Change to your recipient
-		from: 'joshiharsh0506@gmail.com', // Change to your verified sender
-		subject: 'Sending with SendGrid is Fun',
-		text: 'and easy to do anywhere, even with Node.js',
-		html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-	}
-
-	sgMail
-		.send(msg)
-		.then((response) => {
-			console.log(response[0].statusCode)
-			console.log(response[0].headers)
-		})
-		.catch((error) => {
-			console.error(error)
-		})
-}
-
 const url = `mongodb+srv://${process.env.USERNAMES}:${process.env.PASSWORD}@cluster0.dussn.mongodb.net/?retryWrites=true&w=majority`
 // Connect to MongoDB database
 mongoose
